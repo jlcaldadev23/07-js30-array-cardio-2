285377e1bafdb3fdfdf2759a482809180018da60
jlcaldadev23 <jlcaldadev23@gmail.com>
2022-06-18 12:43:09 +0800

    Tried .some .every .find .findIndex and spread operator

---
+++ script.js
@@ -0,0 +1,62 @@
+const people = [
+  { name: "Wes", year: 1988 },
+  { name: "Kait", year: 1986 },
+  { name: "Irv", year: 1970 },
+  { name: "Lux", year: 2015 },
+];
+
+const comments = [
+  { text: "Love this!", id: 523423 },
+  { text: "Super good", id: 823423 },
+  { text: "You are the best", id: 2039842 },
+  { text: "Ramen is my fav food ever", id: 123523 },
+  { text: "Nice Nice Nice!", id: 542328 },
+];
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+
+// const oneIsAdult = people.some((person) => {
+//   const currentYear = new Date().getFullYear();
+//   return currentYear - person.year >= 19;
+// });
+
+const oneIsAdult = people.some(
+  (person) => new Date().getFullYear() - person.year >= 19
+);
+
+console.log(oneIsAdult);
+
+// Array.prototype.every() // is everyone 19 or older?
+
+const allIsAdult = people.every(
+  (person) => new Date().getFullYear() - person.year >= 19
+);
+
+console.log(allIsAdult);
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+
+const specificId = comments.find((comment) => comment.id === 823423);
+
+console.table(specificId);
+// Array.prototype.findIndex()
+
+const specificIndex = comments.findIndex((comment) => comment.id === 823423);
+
+console.log(specificIndex);
+
+// comments.splice(specificIndex, 1);
+// console.table(comments);
+
+const newComments = [
+  ...comments.slice(0, specificIndex),
+  ...comments.slice(specificIndex + 1),
+];
+
+console.table(newComments);
+
+// Find the comment with this ID
+// delete the comment with the ID of 823423
